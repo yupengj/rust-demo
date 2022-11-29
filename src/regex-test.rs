81812dd4b944903371ba9a19ecfc035896df917a
jyp <jyp10@qq.com>
2022-11-29 23:32:02 +0800

    Create README.md

--- src/regex-test.rs
+++ src/regex-test.rs
@@ -3,15 +3,6 @@ extern crate regex;
 use regex::Regex;
 
 fn main() {
-
-    println!("Hello, world!");
-
     let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
     println!("Did our date match? {}", re.is_match("2014-01-01"));
 }
-
-// fn hello(name: &str) {
-//     if 1 == 1 {
-//        // println!("nihao " & name)
-//     }
-// }
