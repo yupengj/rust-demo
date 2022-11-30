64dc1ead456b31b7b88025044437bbd9f35f58df
jyp1 <jyp10@qq.com>
2022-11-30 22:43:30 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,52 @@
-extern crate regex;
-
-use regex::Regex;
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
+    println!("猜数开始!");
 
-    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
-    println!("Did our date match? {}", re.is_match("2014-01-01"));
+    println!("要猜的数是: {}", secret_number);
+
+    println!("请输入一个数1-100之间的正数，输入0放弃继续猜数");
+    println!();
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{}是无效输入请继续输入", guess);
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("猜小了!"),
+            Ordering::Greater => println!("猜大了!"),
+            Ordering::Equal => {
+                println!("猜对了!");
+                break;
+            }
+        }
+
+        print!("您输入的数字是: {}. ", guess);
+        if guess == 0 {
+            println!("你放弃了");
+            break;
+        } else if guess == secret_number {
+            println!("猜对了");
+            break;
+        } else if guess > secret_number {
+            println!("猜大了");
+        } else if guess < secret_number {
+            println!("猜小了");
+        }
+    }
 }
-
-// fn hello(name: &str) {
-//     if 1 == 1 {
-//        // println!("nihao " & name)
-//     }
-// }
